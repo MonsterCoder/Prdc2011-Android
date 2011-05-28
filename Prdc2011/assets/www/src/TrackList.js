4f798633d66a92ab1dd7f1ddc01431b93ff503b6
MonsterCoder <MonsterCoder@gmail.com>
2011-05-28 12:42:18 -0500

    moved stores to storage.js

--- Prdc2011/assets/www/src/TrackList.js
+++ Prdc2011/assets/www/src/TrackList.js
@@ -1,10 +1,3 @@
-tracks = [ ];
-
-trackStore =new Ext.data.JsonStore({
-								            model: 'Track',					
-								            data :  tracks
-								        });
-								        
 prdc.views.TrackList = Ext.extend(Ext.Panel, {
     layout: 'card',
     scroll: 'vertical',
@@ -22,7 +15,7 @@ prdc.views.TrackList = Ext.extend(Ext.Panel, {
             }
         });
         
-        this.list.on('afterrender', this.loadStore, this);
+        this.list.on('afterrender', loadTrackStore, this);
         
         this.items = [this.list];
         
@@ -41,47 +34,7 @@ prdc.views.TrackList = Ext.extend(Ext.Panel, {
 													            
 													            this.setActiveItem(track, 'slide');
 													        }		   
-     },
-     loadStore: function(){
-     	var me  = this.list;
-        me.setLoading(true); 
-        
-       	Ext.Ajax.request({
-						     url: 'http://prairiedevcon.com/Tracks?format=json',
-						     method: "GET",
-						     success: function(res, request) {                
-						                    if (res) {
-						                                  result = Ext.util.JSON.decode(res.responseText);     
-						                          		  for (var i = 0; i < result.length; i++) {
-							                                        track = Ext.ModelMgr.create({
-							                                         			   id: result[i]['Id'],
-																				   category: result[i]['Category'],
-																				   key: result[i]['Key'],
-																				   name: result[i]['Name'],
-																				   numberOfSessions: result[i]['NumberOfSessions']
-																				}, 'Track'); 
-																				 
-																	tracks.push(track);
-														  };
-														  
-						                                  trackStore.add.apply(trackStore, tracks); 	
-						                                  me.setLoading(false); 		                                 
-						               		}
-						               		else {
-						                            alert('There was an error retrieving the data.');
-						                            me.setLoading(false); 				               		
-						                    };
-						                     
-						                   
-						     },
-						     failure: function(res, request){
-						                                 alert('Failed: ', res);
-						                                 me.setLoading(false); 
-						                                 
-						     }
-		});  
-		
-     }
+     }  
 });
 
 Ext.reg('tracklist', prdc.views.TrackList);
